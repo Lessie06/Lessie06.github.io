48b3a9bd0e01705086f91b98db77ee1dce0d1b70
Lessie Quezada <Lessieq07@gmail.com>
2021-12-09 20:00:09 -0500

    Added a form and styling, install emailjs so the form sends to my email.

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,40 +1,67 @@
 import React from "react";
-import '../styling/ContactMe.css'
-import Footer from './Footer'
+import "../styling/ContactMe.css";
+import emailjs from 'emailjs-com';
 
 function ContactMe() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      "service_32hg8qv",
+      "template_6f9kzeo",
+      e.target,
+      "user_OMvHGilaovhjtP8zkC5dH"
+    ).then(res=>{
+        console.log(res)
+    }).catch(err=> console.log(err));
+  };
   return (
-      <div>
-    <div className='container border'
-    style={{marginTop: "50px",
-    width:'50%',
-    backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzmnOtvW_zmy9H3hhS_yjgCMD3l_HIf3b0nQ&usqp=CAU')`,
-    backgroundPosition: 'center',
-    backgroundSize:'cover',
-    marginBottom:"100px"
-    
-    }}
-    
-    >
+    <div className="top-container">
+      <div
+        className="container border"
+        style={{
+          marginTop: "50px",
+          width: "50%",
 
-     <h1 style={{paddingTop:"50px"}} >Connect with Me. </h1>
-     <form className="row" style={{margin: "25px 85px 75px 100px"}}>
-         <label>Name:</label>
-         <input type="text" name="name" className="form-control"></input>
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          // marginBottom:"100px"
+        }}
+      >
+        <h1 style={{ paddingTop: "50px", color: "black" }}>
+          Connect with Me.{" "}
+        </h1>
+        <form
+          className="row"
+          style={{ margin: "25px 85px 75px 100px" }}
+          onSubmit={sendEmail}
+        >
+          <label>Name:</label>
+          <input type="text" name="name" className="form-control"></input>
 
-         <label>Email:</label>
-         <input type="email" name="user_email" className="form-control"></input>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="user_email"
+            className="form-control"
+          ></input>
 
-         <label>Message:</label>
+          <label>Message:</label>
 
-         <textarea name='message' rows='4' className='form-control' style={{marginBottom:"30px"}}></textarea>
-         <button type="submit" value='send' className="form-control btn btn-secondary"  >Submit</button>
-     </form>
-
-     
-    
-    </div>
-    
+          <textarea
+            name="message"
+            rows="4"
+            className="form-control"
+            style={{ marginBottom: "30px" }}
+          ></textarea>
+          <button
+            type="submit"
+            value="send"
+            className="form-control btn btn-secondary"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
